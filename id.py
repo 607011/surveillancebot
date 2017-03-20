ac89bdd2f6fe3e35a84aeef0383e401332cf65b3
Oliver Lau <ola@ct.de>
2017-03-20 17:10:56 +0100

    Artifical delay before reply.

--- id.py
+++ id.py
@@ -10,21 +10,23 @@ bot = None
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print("Message from ID {}".format(chat_id))
-    if content_type == "text":
-        bot.sendMessage(chat_id, "Your ID: {}".format(chat_id))
+    if content_type == 'text':
+        bot.sendChatAction(chat_id, action='typing')
+        time.sleep(1.4)
+        bot.sendMessage(chat_id, 'Your ID: {}'.format(chat_id))
 
 
 def main(api_key):
     global bot
     bot = telepot.Bot(api_key)
     bot.message_loop(handle)
-    print(bot.getMe())
-    print("Listening ...")
+    print('Listening ...')
     while True:
         time.sleep(10)
 
 
-if __name__ == "__main__":
-    main(sys.argv[1])
-
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print('Usage: id.py TELEGRAM_API_KEY')
